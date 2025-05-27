db47a7de72e1e34e3cb59964859035c5cd9160bf
JackieLin2004 <linxy20040304@163.com>
2025-05-28 01:41:14 +0800

    Environment setup

---
+++ src/main/java/ynu/jackielinn/xhs_springboot3/XhsSpringboot3Application.java
@@ -0,0 +1,13 @@
+package ynu.jackielinn.xhs_springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhsSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhsSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ynu/jackielinn/xhs_springboot3/XhsSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ynu.jackielinn.xhs_springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhsSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
